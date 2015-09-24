a6e4fb8670e7b25a0a91e8f69d3b344cd85f1f99
Piotr Łepkowski <ozrabal@gmail.com>
2015-09-24 11:45:36 +0200

    strip gallery shortcode

--- functions.php
+++ functions.php
@@ -68,3 +68,17 @@ function pwp_remove_taxonomy(){
 add_action('init', 'pwp_remove_taxonomy');
 
 
+//strip gallery shortcode from content
+function  strip_shortcode_gallery( $content ) {
+    preg_match_all( '/' . get_shortcode_regex() . '/s', $content, $matches, PREG_SET_ORDER );
+    if( !empty( $matches ) ) {
+        foreach( $matches as $shortcode ) {
+            if( 'gallery' === $shortcode[2] ) {
+                $pos = strpos( $content, $shortcode[0] );
+                if($pos !== false)
+                    return substr_replace( $content, '', $pos, strlen($shortcode[0]) );
+            }
+        }
+    }
+    return $content;
+}
