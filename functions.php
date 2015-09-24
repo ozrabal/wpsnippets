d877830855c803d1265338d9135d966e9cdea0d3
Piotr Łepkowski <ozrabal@gmail.com>
2015-09-24 11:36:28 +0200

    remove admin menu pages

--- functions.php
+++ functions.php
@@ -1,2 +1,8 @@
 <?php
 
+//remove admin menu pages
+function pwp_admin_menu_page_remove() {
+    remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'pwp_admin_menu_page_remove' );
+
